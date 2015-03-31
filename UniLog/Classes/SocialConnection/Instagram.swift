3387ba7da98cc12ec13c22c048580d4e11747021
Robert Bevilacqua <rbevilacqua11@gmail.com>
2015-03-31 11:00:00 -0430

    MVVC Project RB

---
+++ UniLog/Classes/SocialConnection/Instagram.swift
@@ -0,0 +1,73 @@
+//
+//  Instagram.swift
+//  PhotoBrowser
+//
+//  Created by Zhouqi Mo on 12/22/14.
+//  Copyright (c) 2014 Zhouqi Mo. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+
+struct Instagram {
+    
+    enum Router: URLRequestConvertible {
+        static let baseURLString = "https://api.instagram.com"
+        static let clientID = "cc83418c94924bfaae6d5fa02d28968e"
+        static let redirectURI = "http://oauth.net/"
+        static let clientSecret = "206bd79649044a0e9a73e72861cb9372"
+        static let authorizationURL = NSURL(string: Router.baseURLString + "/oauth/authorize/?client_id=" + Router.clientID + "&redirect_uri=" + Router.redirectURI + "&response_type=code")!
+        
+        case PopularPhotos(String, String)
+        case requestOauthCode
+        
+        static func requestAccessTokenURLStringAndParms(code: String) -> (URLString: String, Params: [String: AnyObject]) {
+            let params = ["client_id": Router.clientID, "client_secret": Router.clientSecret, "grant_type": "authorization_code", "redirect_uri": Router.redirectURI, "code": code]
+            let pathString = "/oauth/access_token"
+            let urlString = Instagram.Router.baseURLString + pathString
+            return (urlString, params)
+        }
+        
+        var URLRequest: NSURLRequest {
+            let (path: String, parameters: [String: AnyObject]) = {
+                switch self {
+                case .PopularPhotos (let userID, let accessToken):
+                    let params = ["access_token": accessToken]
+                    let pathString = "/v1/users/" + userID + "/media/recent"
+                    return (pathString, params)
+                    
+                case .requestOauthCode:
+                    let pathString = "/oauth/authorize/?client_id=" + Router.clientID + "&redirect_uri=" + Router.redirectURI + "&response_type=code"
+                    return ("/photos", [:])
+                }
+                }()
+            
+            let BaeseURL = NSURL(string: Router.baseURLString)
+            var URLRequest = NSURLRequest(URL: BaeseURL!.URLByAppendingPathComponent(path))
+            let encoding = Alamofire.ParameterEncoding.URL
+            return encoding.encode(URLRequest, parameters: parameters).0
+        }
+    }
+    
+}
+
+extension Alamofire.Request {
+    class func imageResponseSerializer() -> Serializer {
+        return { request, response, data in
+            if data == nil {
+                return (nil, nil)
+            }
+            
+            let image = UIImage(data: data!, scale: UIScreen.mainScreen().scale)
+            
+            return (image, nil)
+        }
+    }
+    
+    func responseImage(completionHandler: (NSURLRequest, NSHTTPURLResponse?, UIImage?, NSError?) -> Void) -> Self {
+        return response(serializer: Request.imageResponseSerializer(), completionHandler: { (request, response, image, error) in
+            completionHandler(request, response, image as? UIImage, error)
+        })
+    }
+}
+
