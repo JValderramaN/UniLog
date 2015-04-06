ff30b998644575da18363cc217f649b4764ffc1d
Robert Bevilacqua <rbevilacqua11@gmail.com>
2015-04-06 15:29:09 -0430

    oauthio

--- UniLog/Classes/ViewController/ViewController.swift
+++ UniLog/Classes/ViewController/ViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, OAuthIODelegate {
     
+
+    var oauth_modal: OAuthIOModal? = nil
+    var request_object: OAuthIORequest? = nil
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.oauth_modal = OAuthIOModal(key: "sny9pWwsI6LghavyM_hT7Oo2hOw", delegate: self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,9 +29,15 @@ class ViewController: UIViewController {
     //buttons action
 
     @IBAction func twitterButtonTapped(sender: UIButton) {
+
+
     }
 
     @IBAction func instagramButtonTapped(sender: UIButton) {
+        var options = NSMutableDictionary()
+        options.setValue("true", forKey: "cache")
+        self.oauth_modal?.showWithProvider("instagram", options: options)
+
     }
     
     @IBAction func facebookButtonTapped(sender: UIButton) {
@@ -46,6 +57,35 @@ class ViewController: UIViewController {
     
     @IBAction func foursquareButtonTapped(sender: UIButton) {
     }
-    
+
+
+    //oAuth
+    func didReceiveOAuthIOResponse(request: OAuthIORequest!) {
+        var cred: NSDictionary = request.getCredentials()
+        println(cred.objectForKey("access_token")!)
+        //println(cred.objectForKey("oauth_token_secret"))
+       // self.status_label.text = "Logged in with Twitter/Instagram"
+        self.request_object = request
+    }
+
+    func didFailWithOAuthIOError(error: NSError!) {
+        //self.status_label.text = "Could not login with Twitter/Instagram"
+    }
+
+
+    func didFailAuthenticationServerSide(body: String!, andResponse response: NSURLResponse!, andError error: NSError!) {
+
+    }
+
+    func didAuthenticateServerSide(body: String!, andResponse response: NSURLResponse!) {
+
+    }
+
+    func didReceiveOAuthIOCode(code: String!) {
+
+    }
+
+
+
 }
 
